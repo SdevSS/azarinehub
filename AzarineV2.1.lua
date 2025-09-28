5665e453548eadba4851e49a5d85d8d1ea997abd
JustInno <112251715+SdevSS@users.noreply.github.com>
2025-09-28 10:47:40 +0700

    Update AzarineV2.1.lua

--- AzarineV2.1.lua
+++ AzarineV2.1.lua
@@ -1,9 +1,12 @@
 if game.PlaceId == 126244816328678 then
-	print("Loading AzarineHub CDID Script...")
+	print("Loading AzarineHub DIG Script...")
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/SdevSS/azarinehub/refs/heads/main/AzarineHub_DIG.lua"))()
 elseif game.GameId == 2640407187 then
 	print("Loading AzarineHub CDID Script...")
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/SdevSS/azarinehub/refs/heads/main/AzarineHub_CDID.lua"))()
+elseif game.GameID == 6701277882 then
+	print("Loading AzarineHub FISH IT Script...")
+	
 else
 	game.Players.LocalPlayer:Kick("[AzarineHub] Game is not Supported!")
 end
