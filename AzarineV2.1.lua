6313056bff9825c8a6f3dd89c800bba2ebbd0831
JustInno <112251715+SdevSS@users.noreply.github.com>
2025-09-28 11:26:57 +0700

    Update AzarineV2.1.lua

--- AzarineV2.1.lua
+++ AzarineV2.1.lua
@@ -4,7 +4,7 @@ if game.PlaceId == 126244816328678 then
 elseif game.GameId == 2640407187 then
 	print("Loading AzarineHub CDID Script...")
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/SdevSS/azarinehub/refs/heads/main/AzarineHub_CDID.lua"))()
-elseif game.GameID == 6701277882 then
+elseif game.GameId == 6701277882 then
 	print("Loading AzarineHub FISH IT Script...")
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/SdevSS/azarinehub/refs/heads/main/AzarineHub_FISHIT.lua'))()
 else
